36b69373f9e88c1fbf93366f6a2f9c234a624f20
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2025-11-27 15:30:28 +0530

    Update useradd_example.sh

--- scheduler/useradd_example.sh
+++ scheduler/useradd_example.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Create a shared home directory for Test user
 # Make sure to replace the username, gid, and uid with the desired values
-# In Cyclecloud, user is created with the username and uid and gid are 20001
 # We need to make sure that we create the proper uid and gid for the user in scheduler.
 # Author : Vinil Vadakkepurakkal
 # Date : 25/11/2025
@@ -35,3 +34,5 @@ su - $username -c "ssh-keygen -t rsa -N '' -f /shared/home/$username/.ssh/id_rsa
 su - $username -c "cat /shared/home/$username/.ssh/id_rsa.pub >> /shared/home/$username/.ssh/authorized_keys"
 su - $username -c "chmod 600 /shared/home/$username/.ssh/authorized_keys"
 su - $username -c "chmod 700 /shared/home/$username/.ssh"
+echo "alias ll='ls -alF'" >> /etc/profile 
+echo "User $username created with UID $uid and GID $gid"
