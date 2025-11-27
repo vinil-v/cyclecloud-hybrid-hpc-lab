631b97a8ebc490676678f2bb58566903f404a225
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2025-11-27 14:06:50 +0530

    Update 03-partionconf.sh

--- scheduler/03-partionconf.sh
+++ scheduler/03-partionconf.sh
@@ -5,3 +5,4 @@
 # Date : 26/11/2025
 echo "PartitionName=onprem Nodes=compute1 State=UP Default=YES" >> /etc/slurm/azure.conf
 echo "NodeName=compute1 CPUs=4 Boards=1 SocketsPerBoard=1 CoresPerSocket=2 ThreadsPerCore=2 RealMemory=15989" >> /etc/slurm/azure.conf
+scontrol reconfigure
